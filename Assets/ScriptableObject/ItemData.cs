28735fe199903472b0c4abebb6bf5cb8e3512376
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-11-15 16:58:01 +0900

    [Update] ItemData(ScriptableObject)

---
+++ Assets/ScriptableObject/ItemData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum ItemType
+{
+    MoveSpeedUp,
+    Heal
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "NewItem")]
+
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string ItemName;
+    public string ItemDescription;
+    public ItemType ItemType;
+    public int Value;
+}
